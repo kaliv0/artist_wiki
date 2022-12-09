f045741d1457af41d137b7b1cd12b17603a480fe
kaloyan <kaloyan.ivanov88@gmail.com>
2022-12-09 17:15:05 +0200

    added seed script

--- artist_wiki/artists/api/serializers.py
+++ artist_wiki/artists/api/serializers.py
@@ -15,8 +15,8 @@ class ArtworkSerializer(serializers.ModelSerializer):
 
 class ArtistSerializer(serializers.ModelSerializer):
     influences = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
-    # artworks = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
-    artworks = ArtworkSerializer(many=True, read_only=True)
+    artworks = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    # artworks = ArtworkSerializer(many=True, read_only=True)
 
     class Meta:
         model = Artist
@@ -36,7 +36,9 @@ class CountrySerializer(serializers.ModelSerializer):
 
 class StyleSerializer(serializers.ModelSerializer):
     # artists = ArtistSerializer(many=True, read_only=True)
+    # artworks = ArtworkSerializer(many=True, read_only=True)
     artists = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    artworks = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
 
     class Meta:
         model = Style
---
+++ artist_wiki/migrate_and_seed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python3 manage.py migrate
+
+fixtures=$(ls seed/)
+
+while IFS= read -r fixture; do
+  echo -n "Seeding "
+  echo "$fixture"
+  python3 manage.py loaddata seed/"$fixture"
+done <<< "$fixtures"
